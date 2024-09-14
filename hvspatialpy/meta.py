0f4ee8b395b09f7e88b785d9a239dc9c9a021c81
Javier Ornelas <jornela1@g.ucla.edu>
2024-09-14 14:24:33 -0700

    Add files via upload

---
+++ hvspatialpy/meta.py
@@ -0,0 +1,8 @@
+# This file is part of hvspatialpy, a Python package for evaluating
+# spatial variability of a site utilizing spatially distributed HVSR.
+
+# Copyright (c) 2024 Francisco Javier Ornelas (jornela1@g.ucla.edu)
+
+"""metadata for hvspatialpy."""
+
+__version__ = "1.0.0"
