0a7f7bf0593d8cdda126cb9ecdb1f0aa13a3cc15
Javier Ornelas <jornela1@g.ucla.edu>
2024-09-15 22:58:55 -0700

    Delete hvspatialpy/meta.py

--- hvspatialpy/meta.py
+++
@@ -1,8 +0,0 @@
-# This file is part of hvspatialpy, a Python package for evaluating
-# spatial variability of a site utilizing spatially distributed HVSR.
-
-# Copyright (c) 2024 Francisco Javier Ornelas (jornela1@g.ucla.edu)
-
-"""metadata for hvspatialpy."""
-
-__version__ = "1.1.0"
